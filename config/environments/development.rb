74e2caa984d671d88889b36ffe520479fbeeda51
Iain Murray <murray.iain@gmail.com>
2013-07-30 14:18:33 -0700

    Devise Install Login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Omrails::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # In Production, :host should be set to the actual host of yoru application.
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
 end
